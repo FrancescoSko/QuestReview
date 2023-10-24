99a3b79082f6616ee025a7cc7d8fb372cf91e9f8
Michela Del Conte <michela.delconte@digited.it>
2023-10-24 22:07:59 +0200

    metodi per aggiunta, eliminazione e modifica User

--- questReview/src/main/java/com/example/questReview/service/UserService.java
+++ questReview/src/main/java/com/example/questReview/service/UserService.java
@@ -1,7 +1,37 @@
 package com.example.questReview.service;
 
+import com.example.questReview.entity.User;
+import com.example.questReview.repository.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
+    private UserDAO userDao;
+
+    @Autowired
+    public UserService (UserDAO userDao) {
+        this.userDao = userDao;
+    }
+
+    public User insertUser(User user) {
+        return userDao.save(user);
+    }
+
+    public void deleteUserById(Long userId) {
+        userDao.deleteById(userId);
+    }
+
+    public User updateUser(Long userId, User updateduser) {
+        if (userDao.existsById(userId)) {
+            updateduser.setId(userId);
+            return userDao.save(updateduser);
+        } else {
+            throw new IllegalArgumentException("Utente non trovato!");
+        }
+    }
 }
+
