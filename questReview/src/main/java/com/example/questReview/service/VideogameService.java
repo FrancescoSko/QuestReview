a70f7df1246052a7ee651e50ded8e3bc3abd14ab
Alice <fenuwu99@gmail.com>
2023-10-25 14:26:14 +0200

    add logic add videogame remove videogame and uptade videogame into VideogameService

--- questReview/src/main/java/com/example/questReview/service/VideogameService.java
+++ questReview/src/main/java/com/example/questReview/service/VideogameService.java
@@ -1,7 +1,35 @@
 package com.example.questReview.service;
 
+import com.example.questReview.entity.Videogame;
+import com.example.questReview.repository.VideogameDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VideogameService {
+
+    private VideogameDAO videogameDAO;
+
+    @Autowired
+    public VideogameService(VideogameDAO videogameDAO){
+        this.videogameDAO = videogameDAO;
+    }
+
+    public Videogame addVideoGame(Videogame videogame){
+        return videogameDAO.save(videogame);
+    }
+
+    public void removeVideogame(Long id){
+        videogameDAO.deleteById(id);
+    }
+
+    public Videogame uptadeVideogame(Long id, Videogame uptadeVideogame){
+        if (videogameDAO.existsById(id)) {
+            uptadeVideogame.setId(id);
+            return videogameDAO.save(uptadeVideogame);
+        } else {
+            throw new IllegalArgumentException("Videogame ID not found!");
+        }
+    }
 }
+
