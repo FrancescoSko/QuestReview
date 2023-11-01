bec17cdf45b0e3683815f46e08fff31e4ee0ccda
Michela Del Conte <michela.delconte@digited.it>
2023-11-01 12:29:46 +0100

    Added ReviewDto

--- questReview/src/main/java/com/example/questReview/dto/RewiewDto.java
+++ questReview/src/main/java/com/example/questReview/dto/RewiewDto.java
@@ -1,4 +1,33 @@
 package com.example.questReview.dto;
 
+import java.time.LocalDate;
+
 public class RewiewDto {
+    private Long id;
+    private String reviewText;
+    private LocalDate reviewDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public LocalDate getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(LocalDate reviewDate) {
+        this.reviewDate = reviewDate;
+    }
 }
