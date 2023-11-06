2f89aada50138003fe6acf947d0c0f78781e14ed
FrancescoSko <francescobuonora11@gmail.com>
2023-11-06 12:09:30 +0100

    Modifiche di indentazione nella classe videogame

--- questReview/src/main/java/com/example/questReview/service/ReviewService.java
+++ questReview/src/main/java/com/example/questReview/service/ReviewService.java
@@ -16,7 +16,6 @@ public class ReviewService {
     private VideogameDAO videogameDAO;
 
 
-
     //METODO SELECT
     public List<Videogame> getVideogame() {
         return videogameDAO.findAll();
@@ -28,27 +27,35 @@ public class ReviewService {
     public Optional<Videogame> AddVideogame(Videogame videogame) {
         Optional<Videogame> opt = Optional.of(videogame);
         return Optional.of(videogameDAO.save(videogame));
+    }
 
-    public Videogame addVideogame(Videogame videogame) {
+
+
+    public Videogame addVideogame (Videogame videogame){
         return videogameDAO.save(videogame);
     }
 
 
-    //METODO DELETE
-    public void deleteVideogameById(Long id) {
+        //METODO DELETE
+    public void deleteVideogameById (Long id){
         videogameDAO.deleteById(id);
     }
 
 
-    public Videogame editVideogame(Long id, Videogame videogameUpdated) {
+    public Videogame editVideogame (Long id, Videogame videogameUpdated){
         if (videogameDAO.existsById(id)) {
             videogameUpdated.setId(id);
             return videogameDAO.save(videogameUpdated);
-     }   else{
-            throw new IllegalArgumentException("Videogame not found for this id: " + id);
-        }
-
+            } else {
+                throw new IllegalArgumentException("Videogame not found for this id: " + id);
+            }
     }
+
+
+
+
+
+
 
 
 }
