50e349837f4e9a1350cb8516930692dc01e161a5
FrancescoSko <francescobuonora11@gmail.com>
2023-11-08 17:43:21 +0100

    Correzione nella classe Review

--- questReview/src/main/java/com/example/questReview/service/ReviewService.java
+++ questReview/src/main/java/com/example/questReview/service/ReviewService.java
@@ -16,44 +16,6 @@ public class ReviewService {
     private VideogameDAO videogameDAO;
 
 
-    //METODO SELECT
-    public List<Videogame> getVideogame() {
-        return videogameDAO.findAll();
-    }
-
-
-    //METODO INSERT
-
-    public Optional<Videogame> AddVideogame(Videogame videogame) {
-        Optional<Videogame> opt = Optional.of(videogame);
-        return Optional.of(videogameDAO.save(videogame));
-    }
-
-
-
-    public Videogame addVideogame (Videogame videogame){
-        return videogameDAO.save(videogame);
-    }
-
-
-        //METODO DELETE
-    public void deleteVideogameById (Long id){
-        videogameDAO.deleteById(id);
-    }
-
-
-    public Videogame editVideogame (Long id, Videogame videogameUpdated){
-        if (videogameDAO.existsById(id)) {
-            videogameUpdated.setId(id);
-            return videogameDAO.save(videogameUpdated);
-            } else {
-                throw new IllegalArgumentException("Videogame not found for this id: " + id);
-            }
-    }
-
-
-
-
 
 
 
